3d36faa0824c932c13dd66ae249c6a239b2272fd
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-09-05 10:26:32 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Initialize Supabase
 const { createClient } = supabase;
-const supabaseUrl = 'https://YOUR-SUPABASE-URL.supabase.co';
-const supabaseKey = 'YOUR-SUPABASE-KEY';
+const supabaseUrl = 'https://umfhjmrkzsrscskmqugy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtZmhqbXJrenNyc2Nza21xdWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzODYxNTIsImV4cCI6MjA0MDk2MjE1Mn0.YSiQjUm85e84yFflphLch9wQ_G_uWhQ4z6SMhN-Wa_M';
 const supabase = createClient(supabaseUrl, supabaseKey);
